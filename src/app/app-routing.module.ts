1c6c41dfd4eb6604efc3237611e9881e2ddd10e7
ismaelalvesbit <cearaismael1997@gmail.com>
2019-05-14 13:30:45 -0300

    atualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ import { NotfoundComponent } from './admin/erros/notfound/notfound.component';
 import { NotuthorizationComponent } from './admin/erros/notuthorization/notuthorization.component';
 import { ChatComponent } from './admin/views/chat/chat.component';
 import { ProdutosdesativadoComponent } from './admin/views/produtosdesativado/produtosdesativado.component';
+import { CategoriadesativadoComponent } from './admin/views/categoriadesativado/categoriadesativado.component';
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
@@ -40,7 +41,7 @@ const routes: Routes = [
     {path:"produtos",component: ProdutoComponent},
     {path:"produtosDesativados",component: ProdutosdesativadoComponent},
     {path:"categorias", component: CategoriaComponent},
-    {path:"categoriasDesativadas",component: ProdutosdesativadoComponent},
+    {path:"categoriasDesativadas",component: CategoriadesativadoComponent},
     {path:"authorization", component: NotuthorizationComponent},
     {path:"**", component:NotfoundComponent},
   ]}  
